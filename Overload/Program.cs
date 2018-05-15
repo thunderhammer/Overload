119c6aaf656c3a8981f254a884e277ae6b6a5388
Morten Engemand <engemand@hotmail.com>
2018-05-15 14:01:46 +0200

    overload

--- Overload/Program.cs
+++ Overload/Program.cs
@@ -8,8 +8,26 @@ namespace Overload
 {
     class Program
     {
+        static int Beregn(int int1, int int2)
+        {
+            return (int1 + int2);
+        }
+
+        static int Beregn(int int1, int int2, int int3)
+        {
+            return (Beregn(int1, int2)) + int3;
+        }
+
+        static int Beregn(int int1, int int2, int int3, int int4)
+        {
+            return (Beregn(int1, int2, int3) + int4);
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(Beregn(1, 2));
+            Console.WriteLine(Beregn(1, 2, 3));
+            Console.WriteLine(Beregn(1, 2, 4, 5)); 
         }
     }
 }
